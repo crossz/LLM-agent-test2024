3991a258c9c3b7ae77721b1dcda42dac2b10090a
crossz <ukcrossa@gmail.com>
2024-08-28 22:01:07 +0800

    :art: optimize a bit for api init in app.py.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
     
-db = SQLAlchemy()
+db = SQLAlchemy() ## this must be first, even the following import clause
 api = Api()
 migrate = Migrate()
 
@@ -12,10 +12,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
     
+    ## init db, api and migrate
     db.init_app(app)
-    api.init_app(app)
-    # 初始化 API
+    
     from api import init_api
+    api.init_app(app)
     init_api(api)
 
     migrate.init_app(app, db)
